e69f82a271fc843d5bc04071e906de2f967e6ccb
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-10-25 21:40:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import pink from '@material-ui/core/colors/pink';
+
+import AppController from './Router';
+import './assets/css/main.min.css';
+
+const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      dark: '#601e9e',
+      main: '#8a2be2',
+      light: '#a155e7'
+    },
+    secondary: pink,
+    type: colorScheme
+  }
+});
+
+export default function ThemeEngine() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppController />
+    </ThemeProvider>
+  );
+}
